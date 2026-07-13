3de178f6d13412ba849f9b90dc16ddd1f31c648f
ryan <thatvirtualboy@icloud.com>
2026-07-12 20:49:55 -0600

    Allow all card image hosts in production

--- test/card-images-csp.test.js
+++ test/card-images-csp.test.js
@@ -3,7 +3,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 const test = require("node:test");
 
-test("production CSP allows Scrydex card images", () => {
+test("production CSP allows every card-image provider", () => {
   const config = JSON.parse(
     fs.readFileSync(path.join(__dirname, "..", "vercel.json"), "utf8"),
   );
@@ -12,5 +12,24 @@ test("production CSP allows Scrydex card images", () => {
     (header) => header.key === "Content-Security-Policy",
   )?.value;
 
-  assert.match(csp, /img-src[^;]*https:\/\/images\.scrydex\.com/);
+  const requiredImageSources = [
+    "https://images.scrydex.com",
+    "https://images.pokemontcg.io",
+    "https://tcgplayer-cdn.tcgplayer.com",
+    "https://static.tcgcollector.com",
+    "https://firebasestorage.googleapis.com",
+    "https://*.googleusercontent.com",
+  ];
+
+  const imagePolicy = csp
+    .split(";")
+    .map((directive) => directive.trim())
+    .find((directive) => directive.startsWith("img-src "));
+
+  for (const source of requiredImageSources) {
+    assert.ok(
+      imagePolicy.split(/\s+/).includes(source),
+      `img-src must allow ${source}`,
+    );
+  }
 });
